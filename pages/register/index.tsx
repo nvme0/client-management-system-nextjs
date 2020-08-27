c4064d9cad96256a606770dd1343b8291dbcfd6e
nvme0 <jeff@solutions4theweb.com>
2020-08-27 15:29:15 +1000

    On register page, add register & login mutations. Automatically log the user in after successful registration. Wrap page in OfflineFallbackWrapper component.

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import {
   Stack,
@@ -8,15 +9,34 @@ import {
   FormControl,
   FormLabel,
   Input,
-  FormErrorMessage
+  FormErrorMessage,
+  Spinner
 } from "@chakra-ui/core";
+import { useMutation } from "react-query";
+import request from "graphql-request";
 
 import AuthLayout from "layouts/AuthLayout";
 import { Button } from "components/Button";
 import Container from "components/login/Container";
 import Header from "components/login/Header";
-import LoginErrors from "components/login/LoginErrors";
+import LoginErrors, {
+  Props as TLoginErrors
+} from "components/login/LoginErrors";
 import FormBody from "components/login/FormBody";
+import { GQL_REGISTER, GQL_LOGIN } from "gql/Auth";
+import {
+  Register as TRegister,
+  RegisterVariables,
+  Register_register_errors
+} from "gql/__generated__/Register";
+import {
+  Login as TLogin,
+  LoginVariables,
+  Login_login_errors
+} from "gql/__generated__/Login";
+import { setAccessToken } from "lib/accessToken";
+import { useLoggedInState } from "lib/loggedInState";
+import OfflineFallbackWrapper from "components/OfflineFallbackWrapper";
 
 const schema = yup.object().shape({
   email: yup.string().min(3).max(255).email().required(),
@@ -33,11 +53,35 @@ export interface FormInputState {
 }
 
 const Register = () => {
-  const [loginErrors, setLoginErrors] = useState<{
-    type: "none" | "auth" | "network";
-  }>({
+  const router = useRouter();
+  const [loginErrors, setLoginErrors] = useState<TLoginErrors>({
     type: "none"
   });
+
+  const { isLoggedIn, setIsLoggedIn } = useLoggedInState();
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push("/app/services");
+    }
+  }, [isLoggedIn]);
+
+  const [register] = useMutation<
+    TRegister,
+    RegisterVariables,
+    Register_register_errors,
+    () => void
+  >((variables) =>
+    request(process.env.NEXT_PUBLIC_GRAPHQL_URL!, GQL_REGISTER, variables)
+  );
+
+  const [login] = useMutation<
+    TLogin,
+    LoginVariables,
+    Login_login_errors,
+    () => void
+  >((variables) =>
+    request(process.env.NEXT_PUBLIC_GRAPHQL_URL!, GQL_LOGIN, variables)
+  );
 
   const formik = useFormik<FormInputState>({
     initialValues: {
@@ -47,99 +91,138 @@ const Register = () => {
     validationSchema: schema,
     validateOnBlur: true,
     validateOnChange: true,
-    onSubmit: (values) => {
-      console.log({ values });
+    onSubmit: (values, formikHelpers) => {
+      register(values, {
+        onSuccess: ({ register: { payload, errors } }) => {
+          if (errors.length > 0 || !payload) {
+            setLoginErrors({
+              type: "custom",
+              customMessage: `${errors[0]?.path} ${errors[0]?.message}`
+            });
+            formikHelpers.setSubmitting(false);
+            return;
+          }
+          login(values, {
+            onSuccess: ({ login: { payload, errors } }) => {
+              if (errors.length > 0 || !payload) {
+                router.push("/login");
+                return;
+              }
+              setAccessToken(payload);
+              setIsLoggedIn(true);
+            }
+          });
+        },
+        onError: () => {
+          setLoginErrors({ type: "network" });
+        }
+      });
     }
   });
 
   return (
-    <Container>
-      <Header>Create a Free Account</Header>
-      <Box>
-        <Box
-          {...{
-            display: "flex",
-            justifyContent: "center",
-            p: 4
-          }}
-        >
-          <p className="prose">
-            Already have an account?{" "}
-            <Link
-              {...{
-                href: `/login`
-              }}
-            >
-              <a className="prose">Sign In</a>
-            </Link>
-          </p>
+    <OfflineFallbackWrapper>
+      <Container>
+        <Header>Create a Free Account</Header>
+        <Box>
+          <Box
+            {...{
+              display: "flex",
+              justifyContent: "center",
+              p: 4
+            }}
+          >
+            <p className="prose">
+              Already have an account?{" "}
+              <Link
+                {...{
+                  href: `/login`
+                }}
+              >
+                <a className="prose">Sign In</a>
+              </Link>
+            </p>
+          </Box>
+          <form
+            {...{
+              noValidate: true,
+              onSubmit: formik.handleSubmit
+            }}
+          >
+            <FormBody>
+              <Stack {...{ spacing: 4 }}>
+                <FormControl
+                  {...{
+                    isRequired: true,
+                    isInvalid: formik.touched.email && !!formik.errors.email
+                  }}
+                >
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    {...{
+                      placeholder: "Email",
+                      ...formik.getFieldProps("email"),
+                      onInput: () => {
+                        if (loginErrors.type === "auth") {
+                          setLoginErrors({ type: "none" });
+                        }
+                      }
+                    }}
+                  />
+                  <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                </FormControl>
+              </Stack>
+              <Stack {...{ spacing: 4 }}>
+                <FormControl
+                  {...{
+                    isRequired: true,
+                    isInvalid:
+                      formik.touched.password && !!formik.errors.password
+                  }}
+                >
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    {...{
+                      type: "password",
+                      placeholder: "Password",
+                      ...formik.getFieldProps("password"),
+                      onInput: () => {
+                        if (loginErrors.type === "auth") {
+                          setLoginErrors({ type: "none" });
+                        }
+                      }
+                    }}
+                  />
+                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                </FormControl>
+              </Stack>
+              <LoginErrors {...loginErrors} />
+              <Stack {...{ isInline: true, spacing: 1 }}>
+                <Button
+                  {...{
+                    templateStyle: "login",
+                    type: "submit",
+                    disabled: formik.isSubmitting
+                  }}
+                >
+                  {formik.isSubmitting ? (
+                    <Spinner
+                      {...{
+                        thickness: "3px",
+                        speed: "0.65s",
+                        size: "md"
+                      }}
+                    />
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </Stack>
+            </FormBody>
+          </form>
         </Box>
-        <form
-          {...{
-            noValidate: true,
-            onSubmit: formik.handleSubmit
-          }}
-        >
-          <FormBody>
-            <Stack {...{ spacing: 4 }}>
-              <FormControl
-                {...{
-                  isRequired: true,
-                  isInvalid: formik.touched.email && !!formik.errors.email
-                }}
-              >
-                <FormLabel>Email</FormLabel>
-                <Input
-                  {...{
-                    placeholder: "Email",
-                    ...formik.getFieldProps("email"),
-                    onInput: () => {
-                      if (loginErrors.type === "auth") {
-                        setLoginErrors({ type: "none" });
-                      }
-                    }
-                  }}
-                />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
-            </Stack>
-            <Stack {...{ spacing: 4 }}>
-              <FormControl
-                {...{
-                  isRequired: true,
-                  isInvalid: formik.touched.password && !!formik.errors.password
-                }}
-              >
-                <FormLabel>Password</FormLabel>
-                <Input
-                  {...{
-                    placeholder: "Password",
-                    ...formik.getFieldProps("password"),
-                    onInput: () => {
-                      if (loginErrors.type === "auth") {
-                        setLoginErrors({ type: "none" });
-                      }
-                    }
-                  }}
-                />
-                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-              </FormControl>
-            </Stack>
-            <LoginErrors {...{ loginErrors }} />
-            <Stack {...{ isInline: true, spacing: 1 }}>
-              <Button
-                {...{
-                  templateStyle: "login",
-                  type: "submit"
-                }}
-              >
-                Sign Up
-              </Button>
-            </Stack>
-          </FormBody>
-        </form>
-      </Box>
-    </Container>
+      </Container>
+    </OfflineFallbackWrapper>
   );
 };
 
