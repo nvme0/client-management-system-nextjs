ea471c43285d464b53d43892ced7207567f84b2d
nvme0 <jeff@solutions4theweb.com>
2020-08-27 12:59:11 +1000

    Add LoggedInStateProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,20 @@ import GoogleFonts from "next-google-fonts";
 import theme from "theme";
 
 import { OutboxProvider } from "lib/outbox";
+import { LoggedInStateProvider } from "lib/loggedInState";
 
 const MyApp = ({ Component, pageProps }) => {
   const getLayout = Component.getLayout || ((page) => page);
+
   return (
     <>
       <GoogleFonts href="https://fonts.googleapis.com/css?family=Source Sans Pro:300,400,600,700" />
       <ChakraProvider {...{ theme, resetCSS: true }}>
         {getLayout(
           <OutboxProvider>
-            <Component {...pageProps} />
+            <LoggedInStateProvider>
+              <Component {...pageProps} />
+            </LoggedInStateProvider>
           </OutboxProvider>
         )}
       </ChakraProvider>
