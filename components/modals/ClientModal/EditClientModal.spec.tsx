c09bfe35cbf696d68124b62f08fea06ac697c287
nvme0 <jeff@solutions4theweb.com>
2020-09-01 17:05:13 +1000

    Add EditClientModal

---
+++ components/modals/ClientModal/EditClientModal.spec.tsx
@@ -0,0 +1,64 @@
+import { render, cleanup, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import EditClientModal from "./EditClientModal";
+import { ClientModalProps } from ".";
+
+describe("Edit Client Modal", () => {
+  afterEach(cleanup);
+
+  let props: ClientModalProps;
+
+  beforeEach(() => {
+    props = {
+      client: {
+        id: "4089eabc-69fd-4525-9a41-a3a3177cfdee",
+        firstName: "Jimmy",
+        lastName: "",
+        address: "",
+        contactEmail: "",
+        contactNumber: "",
+        notes: "",
+        createdAt: "2019-10-10T00:53:43.549Z",
+        updatedAt: "2019-10-10T00:53:43.549Z"
+      },
+      modalProps: {
+        isOpen: true,
+        onClose: jest.fn()
+      },
+      handleSave: jest.fn((clientInput) => {}),
+      handleDelete: jest.fn()
+    };
+  });
+
+  it("renders", () => {
+    const { asFragment } = render(<EditClientModal {...props} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("calls onClose on close button click", () => {
+    const { getByTestId } = render(<EditClientModal {...props} />);
+    const close = getByTestId(`close-${props.client.id}`);
+    close.click();
+    expect(props.modalProps.onClose).toBeCalledTimes(1);
+  });
+
+  it("calls handleSave on form submission", async () => {
+    const { getByTestId } = render(<EditClientModal {...props} />);
+    const submit = getByTestId(`submit-${props.client.id}`);
+    submit.click();
+
+    await waitFor(() => {
+      expect(props.modalProps.onClose).toBeCalledTimes(1);
+      expect(props.handleSave).toBeCalledTimes(1);
+      expect(props.handleSave).toBeCalledWith(props.client);
+    });
+  });
+
+  it("calls handleDelete on delete button click", () => {
+    const { getByTestId } = render(<EditClientModal {...props} />);
+    const deleteButton = getByTestId(`delete-${props.client.id}`);
+    deleteButton.click();
+    expect(props.handleDelete).toBeCalledTimes(1);
+  });
+});
---
+++ components/modals/ClientModal/EditClientModal.stories.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import { Button } from "components/Button";
+import EditClientModal from "./EditClientModal";
+
+export default {
+  title: "Modals/EditClientModal",
+  component: EditClientModal
+};
+
+export const Default = () => {
+  const [state, setState] = useState<{
+    Modal?: () => JSX.Element;
+  }>({});
+
+  const handleEditClient = () => {
+    const client = {
+      id: uuid(),
+      firstName: "",
+      lastName: "",
+      address: "",
+      contactEmail: "",
+      contactNumber: "",
+      notes: "",
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+    };
+
+    setState({
+      ...state,
+      Modal: () => (
+        <EditClientModal
+          {...{
+            client,
+            modalProps: {
+              isOpen: true,
+              onClose: () => setState({ ...state, Modal: undefined })
+            },
+            handleSave: (clientInput) => {
+              alert("Saved: " + clientInput.id);
+            },
+            handleDelete: () => {
+              alert("Deleted: " + client.id);
+            }
+          }}
+        />
+      )
+    });
+  };
+
+  return (
+    <>
+      <Button
+        {...{
+          templateStyle: "primary-outline",
+          onClick: handleEditClient
+        }}
+      >
+        Open Modal
+      </Button>
+      {state.Modal && <state.Modal />}
+    </>
+  );
+};
