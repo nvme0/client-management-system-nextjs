237d8da74380da861b92551af92f11ec3cfea120
nvme0 <jeff@solutions4theweb.com>
2020-09-01 17:05:00 +1000

    Add CreateClientModal

---
+++ components/modals/ClientModal/CreateClientModal.stories.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import { Button } from "components/Button";
+import CreateClientModal from "./CreateClientModal";
+
+export default {
+  title: "Modals/CreateClientModal",
+  component: CreateClientModal
+};
+
+export const Default = () => {
+  const [state, setState] = useState<{
+    Modal?: () => JSX.Element;
+  }>({});
+
+  const handleCreateClient = () => {
+    setState({
+      ...state,
+      Modal: () => (
+        <CreateClientModal
+          {...{
+            client: {
+              id: uuid(),
+              firstName: "",
+              lastName: "",
+              address: "",
+              contactEmail: "",
+              contactNumber: "",
+              notes: "",
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString()
+            },
+            modalProps: {
+              isOpen: true,
+              onClose: () => setState({ ...state, Modal: undefined })
+            },
+            handleSave: (clientInput) => {
+              alert("Saved: " + clientInput.id);
+            }
+          }}
+        />
+      )
+    });
+  };
+
+  return (
+    <>
+      <Button
+        {...{
+          templateStyle: "primary-outline",
+          onClick: handleCreateClient
+        }}
+      >
+        Open Modal
+      </Button>
+      {state.Modal && <state.Modal />}
+    </>
+  );
+};
