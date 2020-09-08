2b1aad99b68e3d8e9ce3f6923d959aa823ad7155
nvme0 <jeff@solutions4theweb.com>
2020-09-08 10:49:57 +1000

    Add css styles for react-big-calendar & react-datepicker to _app.tsx & .storybook/preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/core";
 import theme from "../theme";
 
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" }
 };
