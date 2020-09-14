d83e050c4da9317ab9aae3c089fefe2146c6b573
nvme0 <jeff@solutions4theweb.com>
2020-09-14 20:25:48 +1000

    Update useQuery and useMutation hooks to fix breaking changes caused by react-query update. Bypass fetcher and use request until fetcher bugs are fixed.

--- lib/outbox/hooks/useMutation.ts
+++ lib/outbox/hooks/useMutation.ts
@@ -1,10 +1,10 @@
-import { useMutation as m, MutationOptions } from "react-query";
+import { useMutation as m, MutationConfig } from "react-query";
 import { RequestDocument } from "graphql-request/dist/types";
 import { useOutbox } from "..";
 
 export const useMutation = <T = any, V = undefined, E = Error, S = unknown>(
   query: RequestDocument,
-  mutationOptions?: MutationOptions<T, V, E, S>
+  mutationOptions?: MutationConfig<T, E, V, S>
 ) => {
   const { fetcher } = useOutbox();
   return m((variables) => fetcher.enqueue(query, variables), mutationOptions);
