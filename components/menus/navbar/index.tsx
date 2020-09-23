ff93c56ee8216a6fcb388c61849b5259dc2c63e2
nvme0 <jeff@solutions4theweb.com>
2020-09-23 17:54:43 +1000

    Add Install to Homescreen button to navbar

--- components/menus/navbar/index.tsx
+++ components/menus/navbar/index.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
 import { Flex, Badge } from "@chakra-ui/core";
+import { Button } from "components/Button";
 
 import { useOnlineState } from "lib/network";
 
 const Navbar = ({ children }) => {
   const { isOnline } = useOnlineState();
+  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    });
+  }, []);
+
   return (
     <Flex
       {...{
         direction: "row",
-        justifyContent: "flex-end",
+        justifyContent: "space-between",
         alignItems: "center",
         px: { base: 1, sm: 4 },
         py: 1,
@@ -18,36 +29,69 @@ const Navbar = ({ children }) => {
         },
         backgroundColor: "#FFF",
         zIndex: 5,
-        position: "relative"
+        position: "relative",
+        minH: "48px"
       }}
     >
-      {isOnline ? (
-        <Badge
+      <Flex
+        {...{
+          direction: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%"
+        }}
+      >
+        <Button
           {...{
-            mr: {
-              base: 2,
-              md: 0
-            },
-            colorScheme: "green",
-            variant: "outline"
+            display: !deferredPrompt ? "none" : undefined,
+            // visibility: !deferredPrompt ? "hidden" : undefined,
+            // disabled: !deferredPrompt ? true : undefined,
+            templateStyle: "primary",
+            onClick: (e) => {
+              if (!!deferredPrompt) {
+                console.log({ deferredPrompt });
+                (deferredPrompt as any).prompt();
+                (deferredPrompt as any).userChoice.then((choiceResult) => {
+                  if (choiceResult.outcome === "accepted") {
+                    console.log("User accepted the A2HS prompt");
+                  }
+                  setDeferredPrompt(null);
+                });
+              }
+            }
           }}
         >
-          Online
-        </Badge>
-      ) : (
-        <Badge
-          {...{
-            mr: {
-              base: 2,
-              md: 0
-            },
-            colorScheme: "red",
-            variant: "outline"
-          }}
-        >
-          Offline
-        </Badge>
-      )}
+          Add to Homescreen
+        </Button>
+        {isOnline ? (
+          <Badge
+            {...{
+              mr: {
+                base: 2,
+                md: 0
+              },
+              colorScheme: "green",
+              variant: "outline"
+            }}
+          >
+            Online
+          </Badge>
+        ) : (
+          <Badge
+            {...{
+              mr: {
+                base: 2,
+                md: 0
+              },
+              colorScheme: "red",
+              variant: "outline"
+            }}
+          >
+            Offline
+          </Badge>
+        )}{" "}
+      </Flex>
+
       {children}
     </Flex>
   );
