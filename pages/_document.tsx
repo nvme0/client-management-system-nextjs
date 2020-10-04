74d7cd0dbd5e559096cd8b06ee6c50c8a7e09891
nvme0 <jeff@solutions4theweb.com>
2020-10-04 16:22:07 +1000

    Add google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ const APP_DESCRIPTION = "An app that manages your clients relations";
 const APP_THEME_COLOR = "#3182ce";
 
 const GA_TRACKING_ID = "UA-179568776-1";
+const GTM_ID = "GTM-MRVZW84";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -32,6 +33,25 @@ class MyDocument extends Document {
               `
             }}
           />
+
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');`
+            }}
+          />
+          {/* End Google Tag Manager */}
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
 
           <meta name="application-name" content={APP_NAME} />
 
